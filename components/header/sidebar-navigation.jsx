ba3dc27398b8bb5d0cb5f24805789e4a6520bf3a
takwa6 <141859147+takwa6@users.noreply.github.com>
2025-11-03 19:52:36 +0100

    Update sidebar-navigation.jsx

--- components/header/sidebar-navigation.jsx
+++ components/header/sidebar-navigation.jsx
@@ -71,23 +71,9 @@ export default function SidebarNavigation() {
                 </Menu>
 
                 {/* social link start */}
-                <h5 className="text-white text-center mt-35 pb-1 d-inline-block ml-3">
-                    Follow me
-                </h5>
+                
 
-                <ul className="social social-bg text-center d-flex mt-10 ml-3">
-                    {social?.map((item, i) => (
-                        <li key={i} className="mr-2 rotate-hover">
-                            <Link
-                                className={`${item.color} text-center pr-0 text-white d-block transition-3 rotate`}
-                                href={item.href}
-                            >
-                                <i className={item.icon} />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                {/* social link end */}
+                
             </Sidebar>
         </div>
     );
